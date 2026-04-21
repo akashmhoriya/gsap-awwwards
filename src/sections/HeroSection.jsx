3f3bf469042ee4604bfac556483c880470ef204f
akashmhoriya <akashmhoriyaofficial@gmail.com>
2026-04-22 00:24:32 +0530

    final bug fixes

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -2,8 +2,17 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { SplitText } from "gsap/all";
 import React from "react";
+import { useMediaQuery } from "react-responsive";
 
 const HeroSection = () => {
+  const isMobile = useMediaQuery({
+    query: "(max-width: 768px)",
+  });
+
+  const isTablet = useMediaQuery({
+    query: "(max-width: 1024px)",
+  });
+
   useGSAP(() => {
     const titleSplit = SplitText.create(".hero-title", {
       type: "chars",
@@ -41,27 +50,44 @@ const HeroSection = () => {
       scrollTrigger: {
         trigger: ".hero-container",
         start: "1% top",
-        end:"bottom top",
-        scrub:true,
+        end: "bottom top",
+        scrub: true,
         // markers: true,
       },
     });
 
     heroTl.to(".hero-container", {
-      rotate:7,
-      scale:0.9,
-      yPercent:30,
-      ease:"power1.inOut"
-    })
+      rotate: 7,
+      scale: 0.9,
+      yPercent: 30,
+      ease: "power1.inOut",
+    });
   });
   return (
     <section className="bg-main-bg">
       <div className="hero-container">
-        <img
-          src="/images/static-img.png"
-          alt="hero-img"
-          className="absolute bottom-0 left-1/2 -translate-x-1/2 object-auto scale-100 md:scale-150"
-        />
+        {isTablet ? (
+          <>
+            {isMobile && (
+              <img
+                src="/images/hero-bg.png"
+                className="absolute bottom-40 size-full object-cover"
+              />
+            )}
+            <img
+              src="/images/hero-img.png"
+              className="absolute bottom-0 left-1/2 -translate-x-1/2 object-auto"
+            />
+          </>
+        ) : (
+          <video
+            src="/videos/hero-bg.mp4"
+            autoPlay
+            muted
+            playsInline
+            className="absolute inset-0 w-full h-full object-cover"
+          />
+        )}
         <div className="hero-content opacity-0">
           <div className="overflow-hidden">
             <h1 className="hero-title">Freaking Delicious</h1>
